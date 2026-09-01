73c092689df20a2b08a15bef921222062aeb8bc3
sosuisen <hidekazu.kubota@gmail.com>
2026-09-02 01:29:42 +0900

    Remove hello world sample code and test

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,5 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "user-story-mapping" is now active!');
-
-	const disposable = vscode.commands.registerCommand('user-story-mapping.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from User Story Mapping!');
-	});
-
-	context.subscriptions.push(disposable);
-}
+export function activate(context: vscode.ExtensionContext) {}
 
 export function deactivate() {}
